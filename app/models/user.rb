9dbef74df87420e9167d995014c74a7edb594d6d
James Fernandez <jffernan@comcast.net>
2018-09-02 00:26:18 -0700

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 
-  validates :name,  presence: true, length: { maximum: 50 }
+  validates :name, length: { maximum: 50 }
 
   before_save { self.email = email.downcase } #AR callback
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
