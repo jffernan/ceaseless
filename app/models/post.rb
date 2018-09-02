be39d10a72f46fafa001135230eb1ee342c70c89
jffernan <jffernan@comcast.net>
2018-09-01 23:03:30 -0700

    post done

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,6 @@ class Post < ApplicationRecord
   validates :body, presence: true,
                     length: { maximum: 140 }
   belongs_to :user
+  scope :published, -> { where(published: true) }
+  scope :unpublished, -> { where(published: false) }
 end
