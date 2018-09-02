9479c76d6ad67721b0d4e25fa1bae42f81e0fd5c
jffernan <jffernan@comcast.net>
2018-09-01 17:58:57 -0700

    testing login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to widgets_path
+      redirect_to user
     else
       redirect_to login_url, alert: "Invalid user/password combination"
     end
@@ -16,5 +16,5 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     redirect_to login_path, notice: "Logged out"
   end
-  
+
 end
