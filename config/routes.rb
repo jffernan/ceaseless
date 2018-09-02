d9710670de50841f4df1edf9dcf7fd94264e705b
jffernan <jffernan@comcast.net>
2018-09-01 17:09:12 -0700

    added Signup Page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   root 'users#home' #home
   get '/about' => 'users#about'
   resources :users
+
+  get '/signup', to: "users#new"
+  post '/signup', to: "users#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
